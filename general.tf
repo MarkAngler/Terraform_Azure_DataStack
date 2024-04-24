dcb7e3d88b354aae912c825c37484235c9f262dc
MarkAngler <>
2024-04-24 07:28:10 -0400

    Update general.tf

--- general.tf
+++ general.tf
@@ -4,7 +4,73 @@ resource "azurerm_resource_group" "rg" {
 
 }
 
+data "azurerm_client_config" "current" {}
 
+resource "azurerm_key_vault" "keyvault" {
+  name                        = "keyvault-${var.region}-${var.environment}"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "premium"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey",
+  ]
+
+  secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set",
+  ]
+
+  certificate_permissions = [
+    "Backup",
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "Purge",
+    "Recover",
+    "Restore",
+    "SetIssuers",
+    "Update",
+  ]
+  }
+}
 # resource "azurerm_data_factory" "adf" {
 #   name                = "adf-${var.environment}-${var.region}"
 #   location            = azurerm_resource_group.rg.location
