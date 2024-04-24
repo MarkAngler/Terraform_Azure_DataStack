169a9afc80487edc1346bc60193804421513ea95
MarkAngler <>
2024-04-24 15:16:45 -0400

    Update azure_sql.tf

--- azure_sql.tf
+++ azure_sql.tf
@@ -48,3 +48,20 @@ resource "azurerm_mssql_database" "mssql-dbexample-d" {
     prevent_destroy = true
   }
 }
+
+resource "azurerm_mssql_database" "mssql-dbexample-q" {
+  name           = "example-db-q"
+  server_id      = azurerm_mssql_server.mssqlserver-q.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+#   max_size_gb    = 4
+#   read_scale     = true
+  sku_name       = "S0"
+#   zone_redundant = true
+  enclave_type   = "VBS"
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
