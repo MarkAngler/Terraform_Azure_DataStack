4d8b5e1883bdc673276a7d5a2335e57f620b3049
MarkAngler <>
2024-04-24 07:28:08 -0400

    Update azure_sql.tf

--- azure_sql.tf
+++ azure_sql.tf
@@ -1,36 +1,35 @@
-resource "azurerm_sql_server" "mssqlserver-d" {
+resource "azurerm_mssql_server" "mssqlserver-d" {
   name                         = "devops-poc-mssqlserver-d"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "saadmin"
-  administrator_login_password = "thisIsDog11"
+  administrator_login          = "missadministrator"
+  administrator_login_password = azurerm_key_vault_secret.sql_administrator_login.value
+  minimum_tls_version          = "1.2"
 
+#   azuread_administrator {
+#     login_username = "AzureAD Admin"
+#     object_id      = "00000000-0000-0000-0000-000000000000"
+#   }
 
 }
 
-resource "azurerm_sql_server" "mssqlserver-q" {
+resource "azurerm_mssql_server" "mssqlserver-q" {
   name                         = "devops-poc-mssqlserver-q"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "saadmin"
-  administrator_login_password = "thisIsDog11"
+  administrator_login          = "missadministrator"
+  administrator_login_password = azurerm_key_vault_secret.sql_administrator_login.value
+  minimum_tls_version          = "1.2"
 
-
-}
-
-
-resource "azurerm_sql_server" "mssqlserver-r" {
-  name                         = "devops-poc-mssqlserver-r"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "saadmin"
-  administrator_login_password = "thisIsDog11"
-
+#   azuread_administrator {
+#     login_username = "AzureAD Admin"
+#     object_id      = "00000000-0000-0000-0000-000000000000"
+#   }
 
 }
+
 
 
 resource "azurerm_mssql_database" "mssql-dbexample-d" {
@@ -38,10 +37,10 @@ resource "azurerm_mssql_database" "mssql-dbexample-d" {
   server_id      = azurerm_mssql_server.mssqlserver-d.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
+#   max_size_gb    = 4
+#   read_scale     = true
   sku_name       = "S0"
-  zone_redundant = true
+#   zone_redundant = true
   enclave_type   = "VBS"
 
   # prevent the possibility of accidental data loss
