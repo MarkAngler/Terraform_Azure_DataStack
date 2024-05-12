a75178ffc424d7d78d3a112c4421bdb3c4873965
MarkAngler <mark.angler@outlook.com>
2024-05-12 10:59:09 -0400

    Update pydeepq_example.py

--- data_quality/pydeepq_example.py
+++ data_quality/pydeepq_example.py
@@ -20,7 +20,7 @@ df = spark.sparkContext.parallelize([
             Row(a="baz", b=3, c=None)]).toDF()
 
 
-
+# Analyzers
 from pydeequ.analyzers import *
 
 analysisResult = AnalysisRunner(spark) \
@@ -31,3 +31,68 @@ analysisResult = AnalysisRunner(spark) \
 
 analysisResult_df = AnalyzerContext.successMetricsAsDataFrame(spark, analysisResult)
 analysisResult_df.show()
+
+# Profile
+from pydeequ.profiles import *
+
+result = ColumnProfilerRunner(spark) \
+    .onData(df) \
+    .run()
+
+for col, profile in result.profiles.items():
+    print(profile)
+
+# Constraint Suggestions
+from pydeequ.suggestions import *
+
+suggestionResult = ConstraintSuggestionRunner(spark) \
+             .onData(df) \
+             .addConstraintRule(DEFAULT()) \
+             .run()
+
+# Constraint Suggestions in JSON format
+print(suggestionResult)
+
+# Constraint Verification
+from pydeequ.checks import *
+from pydeequ.verification import *
+
+check = Check(spark, CheckLevel.Warning, "Review Check")
+
+checkResult = VerificationSuite(spark) \
+    .onData(df) \
+    .addCheck(
+        check.hasSize(lambda x: x >= 3) \
+        .hasMin("b", lambda x: x == 0) \
+        .isComplete("c")  \
+        .isUnique("a")  \
+        .isContainedIn("a", ["foo", "bar", "baz"]) \
+        .isNonNegative("b")) \
+    .run()
+
+checkResult_df = VerificationResult.checkResultsAsDataFrame(spark, checkResult)
+checkResult_df.show()
+
+
+# Repository
+# Save to a Metrics Repository by adding the useRepository() and saveOrAppendResult() calls to your Analysis Runner.
+from pydeequ.repository import *
+from pydeequ.analyzers import *
+
+metrics_file = FileSystemMetricsRepository.helper_metrics_file(spark, 'metrics.json')
+repository = FileSystemMetricsRepository(spark, metrics_file)
+key_tags = {'tag': 'pydeequ hello world'}
+resultKey = ResultKey(spark, ResultKey.current_milli_time(), key_tags)
+
+analysisResult = AnalysisRunner(spark) \
+    .onData(df) \
+    .addAnalyzer(ApproxCountDistinct('b')) \
+    .useRepository(repository) \
+    .saveOrAppendResult(resultKey) \
+    .run()
+
+# To load previous runs, use the repository object to load previous results back in.
+# result_metrep_df = repository.load() \
+#     .before(ResultKey.current_milli_time()) \
+#     .forAnalyzers([ApproxCountDistinct('b')]) \
+#     .getSuccessMetricsAsDataFrame()
